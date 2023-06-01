cbe9f80c8104f0c213d3ff6e0fc199d7a058b646
Qasaaaye <AhmedHusein503@gmail.com>
2023-06-01 11:11:43 -0700

    added divide without operator formula

---
+++ Java/src/Qasaye/DavideNumbers.java
@@ -0,0 +1,19 @@
+package Qasaye;
+
+public class DavideNumbers {
+    public static void main(String[] args) {
+
+        divideTwo(30,4);
+    }
+
+    public static void divideTwo(int num1, int num2){
+
+        int count =0;
+        while (num1> num2){
+            num1 -= num2;
+            count++;
+        }
+        System.out.println(count + " with a remainder of " + num1);
+    }
+}
+
